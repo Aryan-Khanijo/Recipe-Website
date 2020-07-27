129e608ba96293f0fab3232ddfd737dfdb2ae421
Aryan Khanijo <akhanijo12345@gmail.com>
2020-07-27 22:27:54 +0530

    prod

--- src/Recipe-Website/project1/settings/production.py
+++ src/Recipe-Website/project1/settings/production.py
@@ -7,6 +7,6 @@ ALLOWED_HOSTS = []
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = # Enter Later
-EMAIL_HOST_PASSWORD = # Enter Later 
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = '' 
 EMAIL_PORT = 587
